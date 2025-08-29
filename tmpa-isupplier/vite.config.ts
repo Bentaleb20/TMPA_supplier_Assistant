b10a700796cdbf162c9b691845c4dd61e32c4933
Bentaleb20 <134019187+Bentaleb20@users.noreply.github.com>
2025-08-29 15:11:04 +0100

    Update vite.config.ts

--- tmpa-isupplier/vite.config.ts
+++ tmpa-isupplier/vite.config.ts
@@ -1,7 +1,4 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
 export default defineConfig({
+  base: "/TMPA_supplier_Assistant/",
   plugins: [react()],
-base: "/TMPA_supplier_Assistant/",
 });
